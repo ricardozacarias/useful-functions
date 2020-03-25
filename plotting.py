c50d862bf824d9c357f1e6b452ef5145f74300c6
ricardozacarias <ricardo.zsilva@gmail.com>
2020-03-25 16:25:54 +0000

    updated plotting

--- plotting.py
+++ plotting.py
@@ -39,7 +39,7 @@ def default_plot(ax, spines):
 
     return ax
 
-
+# makes a horizontal colorbar with the provided arguments
 def make_hcolorbar(data, units, cmap, filename):
     
     import matplotlib.pyplot as plt
@@ -59,4 +59,60 @@ def make_hcolorbar(data, units, cmap, filename):
     
     # save figure
     plt.savefig(filename)
+    
+# this function manually truncates colormaps in order to get the colors that we want.
+# it has the option of inverting the truncated version aswell.
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100, reverse=False):
+    
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+   
+    if reverse:
+        new_cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(maxval, minval, n))) 
+        
+        arr = np.linspace(0, 50, 100).reshape((10, 10))
+        fig, ax = plt.subplots(ncols=2)
+        ax[0].imshow(arr, interpolation='nearest', cmap=cmap)
+        ax[1].imshow(arr, interpolation='nearest', cmap=new_cmap)
+        plt.show()
+                
+        return new_cmap
+   
+    else:
+        new_cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+        
+        arr = np.linspace(0, 50, 100).reshape((10, 10))
+        fig, ax = plt.subplots(ncols=2)
+        ax[0].imshow(arr, interpolation='nearest', cmap=cmap)
+        ax[1].imshow(arr, interpolation='nearest', cmap=new_cmap)
+        plt.show()
+        
+        return new_cmap
+
+# extract n colors from a colormap and plots the results in a palplot
+def get_colors(cmap, n):
+    
+    import matplotlib as mpl
+    import seaborn as sns
+    import numpy as np
+    
+    cmap = mpl.cm.get_cmap(cmap)
+    rgba = [cmap(x) for x in np.linspace(0.01, 0.99, n)]
+    sns.palplot(sns.color_palette(rgba));    
+    return rgba
+
+# extract a single color from a colormap
+def get_single_color(cmap, n):
+    
+    import matplotlib as mpl   
+    
+    cmap = mpl.cm.get_cmap(cmap)
+    rgba = cmap(n)
+    return rgba
     
